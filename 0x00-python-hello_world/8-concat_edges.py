3b4ffcda4a778d4b652caadde65abd7d43e37caa
Athaba2016 <sumiefb@gmail.com>
2024-01-02 07:41:09 -0800

    8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interactive, interpreted, object-oriented programming\
+ language combines remarkable power with clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(st)
