57ccf7e45488e0e24a667daecd4a169ad8055ab7
Athaba2016 <sumiefb@gmail.com>
2024-04-30 12:53:09 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+#script fetches data
+
 """fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
 if _name_== "_main_":
+
    request = urllib.request.Request(" https://alx-intranet.hbtn.io/status.")
    with urllib.request.urlopen as request
    body = response.read()
