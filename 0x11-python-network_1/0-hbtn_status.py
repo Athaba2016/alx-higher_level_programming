00e3f128d860579b2a02021480d6dc3513c70e4d
Athaba2016 <sumiefb@gmail.com>
2024-04-30 12:49:08 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,3 +9,4 @@ if _name_== "_main_":
    print("Body response:")
    print ("\t- type: {}".format(type(body)))
    print("\t- content: {}".format(body))
+   print("\t- utf8 content: {}".format(body.decode('utf-8')))
