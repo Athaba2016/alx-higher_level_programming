77235bf12e8f3f0d75d1fe4e14096b4a1b3dac08
Athaba2016 <sumiefb@gmail.com>
2024-05-21 05:33:00 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const fs = requires ('fs');
+const fs = require('fs');
 
-fs.readFile(process.agrv[2], 'utf8', function (err,content) {
-	if (error) {
-		console.log(err);
-	} else {
-		process.stdout.write(data);
-	}
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
 });
