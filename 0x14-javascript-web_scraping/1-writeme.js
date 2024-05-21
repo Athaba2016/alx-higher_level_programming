980a34df261cdbc35d26d4486622ce855c23ef2c
Athaba2016 <sumiefb@gmail.com>
2024-05-21 05:41:22 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = requires('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
-	if (error) {
-		console.log(error);
+	if (err) {
+		console.log(err);
 	}
 });
